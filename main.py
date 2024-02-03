7e3fe1b800448e7021af3afd629b0c3ac3a615fa
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-04 00:29:41 +0200

    more faster algorithm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 from sys import argv
+from math import gcd
 
 
 def read_file(path: str) -> list[int]:
@@ -7,14 +8,23 @@ def read_file(path: str) -> list[int]:
         lines = file.readlines()
     return list(map(lambda x: int(x), lines))
 
+def pollards_rho(n):
+    if n % 2 == 0:
+        return 2
+    x = 2; y = 2; d = 1
+    f = lambda x: (x**2 + 1) % n
+    while d == 1:
+        x = f(x)
+        y = f(f(y))
+        d = gcd(abs(x-y), n)
+    return d
 
 def factorize_number(number: int) -> tuple[int, int]:
-    for i in range(2, int(number**0.5) + 1):
-        if number % i:
-            continue
-        else:
-            return (i, number // i)
-    return (number, 1)
+    d = pollards_rho(number)
+    if d == number:
+        return number, 1
+    else:
+        return d, number // d
 
 
 def main() -> None:
