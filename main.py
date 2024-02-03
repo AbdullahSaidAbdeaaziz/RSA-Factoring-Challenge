cb3501169f5af177ada18a192b14784525bbd9dc
AbdullahSaidAbdeaziz <gitHubjoe2@gmail.com>
2024-02-04 00:16:21 +0200

    implement factorize with high speed thx for sqrt

--- main.py
+++ main.py
@@ -8,14 +8,26 @@ def read_file(path: str) -> list[int]:
     return list(map(lambda x: int(x), lines))
 
 
-def factorize_number(number: int) -> tuple(int, int):
-    pass
+def factorize_number(number: int) -> tuple[int, int]:
+    for i in range(2, int(number**0.5) + 1):
+        if number % i:
+            continue
+        else:
+            return (i, number // i)
+    return (number, 1)
 
 
 def main() -> None:
+
+    if len(argv) != 2:
+        print("Usage: factors <file>")
+        return
+
     path_file = argv[1]
-    print(read_file(path=path_file))
-    print("Hello python")
+    numbers = read_file(path_file)
+    for number in numbers:
+        first, second = factorize_number(number)
+        print(f"{number}={second}*{first}")
 
 
 if __name__ == '__main__':
